6f14b9a91f7e5342c76be2432d9d01ed10839341
Tertiium <yang.guozhang@yahoo.com>
2017-01-30 14:05:22 -0200

    Primeiro Commit - MovileCocoaPods

--- MovileCocoaPods/ViewController.swift
+++ MovileCocoaPods/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://codewithchris.com/code/afsample.json", method: .get).responseJSON { (response) in
+            
+            if response.result.isSuccess{
+                if let json = response.result.value{
+                    print(json)
+                }
+                
+            }else{
+                print(response)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
